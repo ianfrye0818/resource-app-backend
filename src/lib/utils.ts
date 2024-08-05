f7740c844582096dd4515f4b7b345151b8e8e006
Ian <ian_frye@outlook.com>
2024-08-05 14:56:19 -0400

    testing-github acitons

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,6 +10,7 @@ export function capitalizeWords(str: string) {
 }
 
 export function generateClientSideUserProperties(user: User) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { password, ...rest } = user;
   return rest;
 }
